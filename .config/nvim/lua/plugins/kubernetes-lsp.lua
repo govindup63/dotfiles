0a6b6073cdf4d308e64b8cc2a605a0dcc1e33c81
Govind <govindup63@gmail.com>
2025-08-10 22:29:06 +0530

    scaled staging deploy to 1

---
+++ .config/nvim/lua/plugins/kubernetes-lsp.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "L3MON4D3/LuaSnip",
+    version = "v2.4.0",
+    build = "make install_jsregexp",
+    dependencies = { "rafamadriz/friendly-snippets" },
+  },
+  { "rafamadriz/friendly-snippets" },
+}
